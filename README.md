18d99f50e0686e735726a67ecd305ff93338c8e9
diebland <bldemolliens@gmail.com>
2021-08-24 23:41:06 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# Project-Mid-Term
+# Classification Problem : Credit card offer analysis
+
+
+## Background
+
+Apart from the other banking and loan services, the bank provides credit card services which is a very important source of revenue.
+
+The bank wants to understand the demographics and other characteristics of the customers accepting a credit card offer and of those not accepting the credit card offer.
+
+Then, the bank designs a focused marketing study, with 18,000 current bank customers including both customers with and without credit card.  This focused approach allows the bank to analyze data for who does and does not respond to the offer, and to use existing demographic data that is already available on each customer.
+
+
+Data: The data set consists of information on 18,000 current bank customers. These are the definitions of data points provided:
+
+Customer Number: A sequential number assigned to the customers (this column is hidden and excluded – this unique identifier will not be used directly).
+Offer Accepted: Did the customer accept (Yes) or reject (No) the offer.
+Reward: The type of reward program offered for the card.
+Mailer Type: Letter or postcard.
+Income Level: Low, Medium or High.
+Bank Accounts Open: How many non-credit-card accounts are held by the customer.
+Overdraft Protection: Does the customer have overdraft protection on their checking account(s) (Yes or No).
+Credit Rating: Low, Medium or High.
+Credit Cards Held: The number of credit cards held at the bank.
+Homes Owned: The number of homes owned by the customer.
+Household Size: Number of individuals in the family.
+Own Your Home: Does the customer own their home? (Yes or No).
+Average Balance: Average account balance (across all accounts over time). Q1, Q2, Q3 and Q4
+Balance: Average balance for each quarter in the last year
+
+
+## Objectives
+
+The task is to build a model that will provide insight into why some bank customers accept credit card offers. There are also other potential areas of opportunities that the bank wants to understand from the data.
+
+we will use different tools to analyze the data :
+
+SQL, tableau and statistics analysis with Python.
+
+## Our steps
+
+1. SQL 
+
+The project includes SQL part to explore the dataset we have been provided with.
+
+We will work with MySQLWorkbench. This work can be found on the file. link of the file
+
+
+2. Tableau
+
+Vizualization of the data. link of the file or include images
+
+3. Classification model
+
+3.1. Exploring the data
+3.2 Modeling
+
+We will use different models to compare the accuracies and find the model that best fits our data. 
+Then, we will compare the different models using accuracy measures.
+
+4. Bonus : Write three or more functions that can be reused for another project. This work can be found on the file : link of the file
+
+## Conclusion
+
+
+## Documentation
+
+Original and cleaned datasets as well as the graphs used in this project can be found on the folder documentation
+
+Sources https://www.kaggle.com/ekrembayar/fifa-21-complete-player-dataset)
+
+List of libraries (with a link to the documentation) https://scikit-learn.org/stable/index.html
+
+https://docs.scipy.org/doc/scipy/reference/index.html
+
+https://pandas.pydata.org/
+
+https://numpy.org/doc/
+
+https://matplotlib.org/3.1.1/
+
+https://seaborn.pydata.org
+
+
--- SQL-CreditCardClassification.sql
+++ SQL-CreditCardClassification.sql
@@ -26,7 +26,7 @@ SELECT customer_number FROM credit_card_data
 ORDER BY average_balance DESC;
 
 -- average_balance of all the customers
-SELECT AVG(average_balance) FROM credit_card_data;
+SELECT ROUND(AVG(average_balance),2) FROM credit_card_data;
 
 -- GROUP BY queries
 -- average_balance of the customers grouped by `Income Level` Use an alias to change the name 'average_balance'.
@@ -38,6 +38,95 @@ GROUP BY bank_accounts_open;
 -- average number of credit cards held by customers for each of the credit card ratings. Use an alias to change the name 'number of credit cards held'.
 SELECT credit_rating, AVG(credit_cards_held) as average_card FROM credit_card_data
 GROUP BY credit_rating;
+-- Is there any correlation between the columns `credit_cards_held` and `number_of_bank_accounts_open`?
+-- by grouping the data by one of the variables and then aggregating the results of the other column. 
+-- Visually check if there is a positive correlation or negative correlation or no correlation
+SELECT credit_cards_held, 
+COUNT(*) OVER (PARTITION BY bank_accounts_open) 
+FROM credit_card_data
+GROUP BY credit_cards_held
+ORDER BY credit_cards_held ;
+
+SELECT bank_accounts_open, 
+COUNT(*) OVER (PARTITION BY credit_cards_held) 
+FROM credit_card_data
+GROUP BY bank_accounts_open
+ORDER BY bank_accounts_open ;
+SELECT bank_accounts_open, credit_cards_held
+FROM credit_card_data;
+
+-- You might also have to check the number of customers in each category (ie number of credit cards held) 
+-- to assess if that category is well represented in the dataset to include it in your analysis. 
+-- For eg. If the category is under-represented as compared to other categories, ignore that category in this analysis
+SELECT credit_cards_held, COUNT(customer_number)
+FROM credit_card_data
+GROUP BY credit_cards_held
+ORDER BY credit_cards_held;
+-- Only 515 customers hold 4 credit cards. This category can be ignored for our analysis.
+
+-- Find the customers with the following categories :
+-- Credit rating medium or high
+-- Credit cards held 2 or less
+-- Owns their own home
+-- Household size 3 or more
+SELECT * FROM credit_card_data
+WHERE credit_rating IN ("Medium","HIGH")
+AND credit_cards_held <=2
+AND own_your_home = "Yes"
+AND household_size >=3;
+-- filter those customers who have accepted the offer
+SELECT customer_number, offer_accepted FROM credit_card_data
+WHERE credit_rating IN ("Medium","HIGH")
+AND credit_cards_held <=2
+AND own_your_home = "Yes"
+AND household_size >=3
+AND offer_accepted = "Yes";
+
+-- Find the list of customers whose average balance is less than the average balance of all the customers in the database.
+SELECT customer_number
+FROM credit_card_data
+WHERE average_balance < (SELECT ROUND(AVG(average_balance),2) FROM credit_card_data);
+
+-- And create a view to save this query
+create or replace view Customers__Balance_View1 as
+SELECT customer_number
+FROM credit_card_data
+WHERE average_balance < (SELECT ROUND(AVG(average_balance),2) FROM credit_card_data);
+
+-- Find the number of people who accepted the offer vs number of people who did not.
+SELECT COUNT(offer_accepted) offer_accepted FROM credit_card_data
+WHERE offer_accepted = "Yes";
+SELECT COUNT(offer_accepted) offer_rejected FROM credit_card_data
+WHERE offer_accepted = "No";
+
+-- Find the difference in average balances of the customers with high credit card rating and low credit card rating
+SELECT ROUND(AVG(average_balance),2), credit_rating FROM credit_card_data
+GROUP BY credit_rating;
+
+create or replace view Customers__Balance_high as
+SELECT ROUND(AVG(average_balance),2) avg_balance_high
+FROM credit_card_data
+WHERE credit_rating = "High";
+
+create or replace view Customers__Balance_medium as
+SELECT ROUND(AVG(average_balance),2) avg_balance_medium
+FROM credit_card_data
+WHERE credit_rating = "Medium";
+
+
+SELECT Customers__Balance_high.customer, avg_balance_high, avg_balance_medium, Customers__Balance_high.avg_balance,  Customers__Balance_medium.avg_balance
+FROM Customers__Balance_high
+LEFT JOIN Customers__Balance_medium
+ON Customers__Balance_high.avg_balance= Customers__Balance_medium.avg_balance;
 
 
 
+-- Find all types of communication (mailer_type) that were used and with how many customers
+SELECT mailer_type, COUNT(customer_number)
+FROM credit_card_data
+GROUP BY mailer_type;
+
+-- Provide the details of the customer that is the 11th least Q1_balance in your database.
+SELECT*,
+DENSE_RANK() over (ORDER by q1_balance) AS Customer_with_11thleast_Q1Balance
+FROM credit_card_data;
